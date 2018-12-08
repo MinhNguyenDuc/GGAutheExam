af5ade4e6113ccbe0061365c91ed5cf12da7ea31
Minh Nguyen Duc <nguyenducminh24101998@gmail.com>
2018-12-08 19:10:38 +0700

    Submit

---
+++ GGAuthe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GGAuthe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
